145d7928fd5dc5eacf65c06976894e76f3a44396
Omkar Oak <113323073+omkaroak26@users.noreply.github.com>
2023-05-24 20:40:48 +0530

    Update qt.h

--- qt.h
+++ qt.h
@@ -1,10 +1,11 @@
-typedef struct colour_pixel{
+typedef struct colour_pixel
+{
     unsigned char R,G,B;
 }colour_pixel;
 
-typedef  struct Quad_Node{
+typedef  struct Quad_Node
+{
     unsigned char R, G ,B;
-    unsigned int A; //AREA
 
     struct Quad_Node *top_left;
     struct Quad_Node *top_right;
@@ -13,8 +14,8 @@ typedef  struct Quad_Node{
 }Quad_Node;
 
 
-//   functions for compression
-void Add_node(Quad_Node **root , colour_pixel p , int l);
+//functions for compression
+void Add_node(Quad_Node **root , colour_pixel p);
 int Uniformity(colour_pixel **mat , int x ,int y , int l , colour_pixel *p);
 void Compression(Quad_Node ** root , int x , int y, int L, colour_pixel ** mat,int threshold , int *no_of_colour, int *no_of_nodes);
 int Depth(Quad_Node* root);
@@ -24,18 +25,16 @@ void del_Matrix(colour_pixel **mat, int Height);
 void del_tree(Quad_Node *root);
 int powTwo(int x);
 
-//   to convert tree  to matrix
+//to convert tree  to matrix
 void pix_value(colour_pixel **mat,int x,int y,int l,unsigned char R, unsigned char G , unsigned char B);
 void create_matrix(Quad_Node *root , colour_pixel **mat , int x , int y, int l , int *no_of_colour , int no_of_nodes);
 void create_image(colour_pixel **mat , int Height, int max_no_of_colour,char *name_of_file);
 
 //  flipping an image
+void CreateQuadTree(Quad_Node ** root , int x , int y, int L, colour_pixel ** mat, int *no_of_colour, int *no_of_nodes);
+//void printQuadTree(Quad_Node* root);
 void Horizontal_flip(Quad_Node **root);
 void Vertical_flip(Quad_Node **root);
-
-//  overlapping
-void complete_tree(Quad_Node **root,int l, int depth, int line);
-void Overlap_imgs(Quad_Node *root1 , Quad_Node *root2, colour_pixel **mat , int x,int y, int L);
 
 //  filters
 void red(Quad_Node *root,colour_pixel **mat,int x,int y,int L);
