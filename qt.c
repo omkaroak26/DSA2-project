208f975a0b236b9e3aa31787b2598c177f9e3bc8
Omkar Oak <113323073+omkaroak26@users.noreply.github.com>
2023-05-24 20:40:33 +0530

    Update qt.c

--- qt.c
+++ qt.c
@@ -6,13 +6,12 @@
 
 //mat = 2D array in which each element is a color_pixel (that is each element has R G and B)
 
-void Add_node(Quad_Node **root , colour_pixel p , int l) 
+void Add_node(Quad_Node **root , colour_pixel p) 
 {
     Quad_Node *q=(Quad_Node *)malloc(sizeof(Quad_Node));
     q->R=p.R;
     q->G=p.G;
     q->B=p.B;
-    q->A=l*l;    //l is the length of the image (in pixels)
     q->top_left=NULL;
     q->top_right=NULL;
     q->bottom_right=NULL;
@@ -45,9 +44,7 @@ int Uniformity(colour_pixel **mat , int x ,int y , int L , colour_pixel *RGBavg)
     //x(row) and y(column) is the size of mat(2D array)
     long long variance=0, average=0;
     int i , j;
-    long long AvgR=0, AvgG=0 ,AvgB=0 ; 
-
-    long long size = L;     //L = length
+    long long AvgR=0, AvgG=0 ,AvgB=0; 
     
 	for(i = x; i < x + L; i++)
     {
@@ -64,16 +61,14 @@ int Uniformity(colour_pixel **mat , int x ,int y , int L , colour_pixel *RGBavg)
     AvgG = AvgG/(L * L);
     AvgB = AvgB/(L * L);	
 
-    //  storing the Avg values of RGB into  colour_pixel RGBavg
+    //storing the Avg values of RGB into  colour_pixel RGBavg
     RGBavg->R = AvgR;
     RGBavg->G = AvgG;
     RGBavg->B = AvgB;
 
     //printf("%d %d %d\n",AvgR,AvgG,AvgB);
     
-    //  calculating var value of a node
-
-    //  WE ARE not HERE (19-05-2023)
+    //calculating variance value of a node
 
 	for(i = x; i < x + L; i++)
     {
@@ -90,29 +85,28 @@ int Uniformity(colour_pixel **mat , int x ,int y , int L , colour_pixel *RGBavg)
 
 }
 
-//   Compression  in a quadtree of pixels that have RGB data
+//Compression  in a quadtree of pixels that have RGB data
 void Compression(Quad_Node** root , int x , int y, int L, colour_pixel** mat,int threshold , int *no_of_colour, int *no_of_nodes)
 {
-    int average;    // to get the average value (to check for uniformity)
-    
     colour_pixel RGBavg;   // it stores the average colour of current partition...
 
-    average = Uniformity(mat, x, y, L, &RGBavg);
-    //printf("%d ",average);
+    int average = Uniformity(mat, x, y, L, &RGBavg);
 
     if(L<1)
+    {
         return;
+    }
 
-    //   count no of nodes and add the node to the root..
+    //count no of nodes and add the node to the root..
     (*no_of_nodes)=(*no_of_nodes)+1;
 
-    Add_node(root,RGBavg,L);   //add the node in quadtree
+    Add_node(root,RGBavg);   //add the node in quadtree
 
     if(average>threshold)   //base case of recursion
     {
 
         Compression(&(*root)->top_left,     x,      y,      L/2,  mat,    threshold,  no_of_colour,no_of_nodes);
-        (*no_of_colour)+=1  ;
+        (*no_of_colour)+=1;
 
         Compression(&(*root)->top_right,    x,      y+L/2 , L/2,  mat,    threshold, no_of_colour, no_of_nodes);
         (*no_of_colour)+=1;
@@ -125,13 +119,17 @@ void Compression(Quad_Node** root , int x , int y, int L, colour_pixel** mat,int
 
     }
 
-}// we are here 
+}
 
 
-int Depth(Quad_Node* root){    // determining the depth of a quaternary tree
+int Depth(Quad_Node* root)  // determining the depth of a quadtree
+{    
     if (root == NULL)
+    {
         return 0;
-    else{       
+    }
+    else
+    {       
         int t_l = Depth(root->top_left);
         int t_r = Depth(root->top_right);
         int b_r = Depth(root->bottom_right);
@@ -144,25 +142,29 @@ int Depth(Quad_Node* root){    // determining the depth of a quaternary tree
     		max = b_r;
     	if(b_l > max)
     		max = b_l;
+
     	return(max+1);
     }
 }
 
 
-
-
-void del_Matrix(colour_pixel **mat, int Height){
+void del_Matrix(colour_pixel **mat, int Height)
+{
     int i;
-    for(i=0;i<Height;i++){
+    for(i=0;i<Height;i++)
+    {
         free(mat[i]);
     }
     free(mat);
 }
 
-void del_tree(Quad_Node *root){
+void del_tree(Quad_Node *root)
+{
     if(root==NULL)
+    {
         return;
-    
+    }
+        
     del_tree(root->top_left);
     del_tree(root->top_right);
     del_tree(root->bottom_right);
@@ -173,78 +175,134 @@ void del_tree(Quad_Node *root){
 }
 
 
-
-
-
-int powTwo(int x){       //function for obtaining a number ie a power of 2
+int powTwo(int x)  //function for obtaining power of 2 of a number
+{       
 	int a = 2;
 	while(a * a != x)
-		a = a * 2;
+    {
+        a = a * 2;
+    }
+
 	return a;
 }
 
 
-void pix_value(colour_pixel **mat,int x,int y,int l,unsigned char R, unsigned char G , unsigned char B){
+void pix_value(colour_pixel **mat,int x,int y,int l,unsigned char R, unsigned char G , unsigned char B)
+{
 	int i,j;
-
     //  giving same rgb values to a node
 	for(i = x; i < x+l; i++)
-		for(j = y; j < y+l; j++){
-				mat[i][j].R = R;
-				mat[i][j].G = G;
-				mat[i][j].B = B;
+    {
+        for(j = y; j < y+l; j++)
+        {
+            mat[i][j].R = R;
+            mat[i][j].G = G;
+            mat[i][j].B = B;
 		}
+    }
+		
 }
 
-
-void create_matrix(Quad_Node *root , colour_pixel **mat , int x , int y, int L , int *no_of_colour , int no_of_nodes){
+//creates matrix from quadtree
+//each block in the matrix holds the RGB value of the corresponding leaf node in the quadtree
+void create_matrix(Quad_Node *root , colour_pixel **mat , int x , int y, int L , int *no_of_colour , int no_of_nodes)
+{
     if((*no_of_colour)==0 || L==0)
+    {
         return;
+    }
     
     if (root->top_left==NULL)
     {
         pix_value(mat,x,y,L,root->R, root->G , root->B);
         *no_of_colour = (*no_of_colour) - 1;
     }
-    else{
+    else
+    {
         create_matrix(root->top_left ,      mat , x     , y     , L/2 , no_of_colour , no_of_nodes);
         create_matrix(root->top_right ,     mat , x     , y+L/2 , L/2 , no_of_colour , no_of_nodes);
         create_matrix(root->bottom_right ,  mat , x+L/2 , y+L/2 ,L/2 , no_of_colour , no_of_nodes);
         create_matrix(root->bottom_left ,   mat , x+L/2 , y     , L/2 , no_of_colour , no_of_nodes);
     }
-    
-} //creates matrix from quadtree
-//each block in the matrix holds the RGB value of the corresponding leaf node in the quadtree
+}
 
-
-void create_image(colour_pixel **mat , int Height, int max_no_of_colour,char *name_of_file){
+//rewrite ppm file from a matrix
+void create_image(colour_pixel **mat , int Height, int max_no_of_colour,char *name_of_file)
+{
 	
     FILE *fp = fopen(name_of_file,"wb");
 	fprintf(fp, "%s\n", "P6");
 	fprintf(fp, "%d %d\n", Height, Height);
     fprintf(fp, "%d\n", max_no_of_colour);
-
-    int i,j;
   
-	for(i = 0; i < Height; i++){
+	for(int i = 0; i < Height; i++)
+    {
 	 	fwrite(mat[i],sizeof(colour_pixel),Height,fp);
-        printf("%c ",mat[i][0]);
 	}
 	fclose(fp);
-} //rewrite ppm file
+}
 
 
-void Horizontal_flip(Quad_Node **root){
-    if((*root)==NULL)
+void CreateQuadTree(Quad_Node** root , int x , int y, int L, colour_pixel** mat, int *no_of_colour, int *no_of_nodes)
+{
+    if(L<1)
+    {
         return;
+    }
+        
+    //count no of nodes and add the node to the root..
+    (*no_of_nodes)=(*no_of_nodes)+1;
 
-	if((*root)->top_left != NULL){
+
+    Add_node(root,mat[x][y]);   //add the node in quadtree
+
+    CreateQuadTree(&(*root)->top_left,     x,      y,      L/2,  mat,  no_of_colour,no_of_nodes);
+    (*no_of_colour)+=1  ;
+
+    CreateQuadTree(&(*root)->top_right,    x,      y+L/2 , L/2,  mat, no_of_colour, no_of_nodes);
+    (*no_of_colour)+=1;
+
+    CreateQuadTree(&(*root)->bottom_right, x+L/2 , y+L/2 , L/2,  mat,  no_of_colour,no_of_nodes);
+    (*no_of_colour)+=1;
+
+    CreateQuadTree(&(*root)->bottom_left,  x+L/2,  y,      L/2,  mat,  no_of_colour,no_of_nodes);
+    (*no_of_colour)+=1;
+}
+
+//auxillary function : not used
+void printQuadTree(Quad_Node* root) 
+{
+    if (root == NULL) 
+    {
+        return;
+    }
+
+    printf("Value: %d %d %d\n",root->R,root->G,root->B);
+
+    printQuadTree(root->top_left);
+    printQuadTree(root->top_right);
+    printQuadTree(root->bottom_left);
+    printQuadTree(root->bottom_right);
+}
+
+
+void Horizontal_flip(Quad_Node **root)
+{
+    if((*root)==NULL)
+    {
+        return;
+    }
+        
+	if((*root)->top_left != NULL)
+    {
 		Quad_Node *temp = NULL;
 		
+        // Swap the top_left and top_right children
 		temp = (*root)->top_left; 
 		(*root)->top_left = (*root)->top_right;
 		(*root)->top_right = temp;
 		
+        // Swap the bottom_left and bottom_right children
 		temp = (*root)->bottom_left; 
 		(*root)->bottom_left = (*root)->bottom_right;
 		(*root)->bottom_right = temp;
@@ -254,75 +312,53 @@ void Horizontal_flip(Quad_Node **root){
 		Horizontal_flip(&(*root)->bottom_right);
 		Horizontal_flip(&(*root)->bottom_left);
 	}
+    return;
 }
 
-void Vertical_flip(Quad_Node **root){
-    if((*root)==NULL)
-        return;
 
-	if((*root)->top_left != NULL){
-		Quad_Node *temp = NULL;
-		
-		temp = (*root)->top_left; 
-		(*root)->top_left = (*root)->bottom_left;
-		(*root)->bottom_left = temp;
-		
-		temp = (*root)->top_right; 
-		(*root)->top_right = (*root)->bottom_right;
-		(*root)->bottom_right = temp;
+void Vertical_flip(Quad_Node **root)
+{
+    if((*root)==NULL)
+    {
+        return;
+    }
+
+	if((*root)->top_left != NULL)
+    {
+        Quad_Node *temp = NULL;
+
+		// Swap the top_left and bottom_left children
+        temp = (*root)->top_left;
+        (*root)->top_left = (*root)->bottom_left;
+        (*root)->bottom_left = temp;
+
+        // Swap the top_right and bottom_right children
+        temp = (*root)->top_right;
+        (*root)->top_right = (*root)->bottom_right;
+        (*root)->bottom_right = temp;
 
 		Vertical_flip(&(*root)->top_left);
 		Vertical_flip(&(*root)->top_right);
 		Vertical_flip(&(*root)->bottom_right);
 		Vertical_flip(&(*root)->bottom_left);
 	}
+    return;
 }
 
-void complete_tree(Quad_Node **root,int l, int depth, int line){
-    if(*root==NULL)
-        return;
-
-	if(line < depth && (*root)->top_left == NULL){	
-		colour_pixel p;
-		p.R = (*root)->R;
-		p.G = (*root)->G;
-		p.B = (*root)->B;
-
-		Add_node(root,p,l);
-		Add_node(root,p,l);
-		Add_node(root,p,l);
-		Add_node(root,p,l);
-    }
-
-    complete_tree(&(*root)->top_left,       l/2,    depth,  line+1);
-    complete_tree(&(*root)->top_right,      l/2,    depth,  line+1);
-    complete_tree(&(*root)->bottom_right,   l/2,    depth,  line+1);
-    complete_tree(&(*root)->bottom_left,    l/2,    depth,  line+1);
-
-}
-
-/*
-void Overlap_imgs(Quad_Node *root1 , Quad_Node *root2, colour_pixel **mat , int x,int y, int L){
+//obtained by setting red as it is, all remaining values to 0
+void red(Quad_Node *root,colour_pixel **mat,int x,int y,int L)
+{
     if(L==0)
+    {
         return;
-    if(root1->top_left==NULL){
-        pix_value(mat , x, y, L, (int)(root1->R + root2->R)/2 , (int)(root1->G + root2->G)/2, (int)(root1->B + root2->B)/2) ;
     }
-    else{
-        Overlap_imgs(root1->top_left     , root2->top_left     , mat , x     , y     , L/2);
-        Overlap_imgs(root1->top_right    , root2->top_right    , mat , x     , y+L/2 , L/2);
-        Overlap_imgs(root1->bottom_left , root2->bottom_right , mat , x+L/2 , y     , L/2);
-        Overlap_imgs(root1->bottom_right  , root2->bottom_left  , mat , x+L/2 , y+L/2 , L/2);
-    }
-}*/
-
-void red(Quad_Node *root,colour_pixel **mat,int x,int y,int L){
-    if(L==0)
-        return;
+       
     if(root->top_left==NULL)
+    {
         pix_value(mat,x,y,L,root->R,0,0);
-    
-    else{
+    }
+    else
+    {
         red(root->top_left      ,mat, x      ,y      ,L/2);
         red(root->top_right     ,mat, x      ,y+L/2  ,L/2);
         red(root->bottom_left   ,mat, x+L/2  ,y      ,L/2);
@@ -330,13 +366,20 @@ void red(Quad_Node *root,colour_pixel **mat,int x,int y,int L){
     }
 }
 
-void green(Quad_Node *root,colour_pixel **mat,int x,int y,int L){
+//obtained by setting green as it is, all remaining values to 0
+void green(Quad_Node *root,colour_pixel **mat,int x,int y,int L)
+{
     if(L==0)
+    {
         return;
+    }
+
     if(root->top_left==NULL)
+    {
         pix_value(mat,x,y,L,0,root->G,0);
-    
-    else{
+    }
+    else
+    {
         green(root->top_left      ,mat, x      ,y      ,L/2);
         green(root->top_right     ,mat, x      ,y+L/2  ,L/2);
         green(root->bottom_left   ,mat, x+L/2  ,y      ,L/2);
@@ -344,13 +387,20 @@ void green(Quad_Node *root,colour_pixel **mat,int x,int y,int L){
     }
 }
 
-void blue(Quad_Node *root,colour_pixel **mat,int x,int y,int L){
+//obtained by setting blue as it is, all remaining values to 0
+void blue(Quad_Node *root,colour_pixel **mat,int x,int y,int L)
+{
     if(L==0)
+    {
         return;
+    }
+
     if(root->top_left==NULL)
+    {
         pix_value(mat,x,y,L,0,0,root->B);
-    
-    else{
+    }
+    else
+    {
         blue(root->top_left      ,mat, x      ,y      ,L/2);
         blue(root->top_right     ,mat, x      ,y+L/2  ,L/2);
         blue(root->bottom_left   ,mat, x+L/2  ,y      ,L/2);
@@ -359,16 +409,21 @@ void blue(Quad_Node *root,colour_pixel **mat,int x,int y,int L){
 }
 
 
-//   greyscale is obtained by multiplying  R * 0.299 , G * 0.578 , B* 0.114
-void grey(Quad_Node *root,colour_pixel **mat,int x,int y,int L){
+//greyscale is obtained by multiplying  R * 0.299 , G * 0.578 , B* 0.114
+void grey(Quad_Node *root,colour_pixel **mat,int x,int y,int L)
+{
     if(L==0)
+    {
         return;
+    }
 
-    if(root->top_left==NULL){
+    if(root->top_left==NULL)
+    {
         int c=(root->R)*0.299 + (root->G)*0.587 + (root->B)*0.114;
         pix_value(mat,x,y,L,c,c,c);
     }
-    else{
+    else
+    {
         grey(root->top_left      ,mat, x      ,y      ,L/2);
         grey(root->top_right     ,mat, x      ,y+L/2  ,L/2);
         grey(root->bottom_left   ,mat, x+L/2  ,y      ,L/2);
@@ -376,22 +431,31 @@ void grey(Quad_Node *root,colour_pixel **mat,int x,int y,int L){
     }
 }
 
-void black_white(Quad_Node *root,colour_pixel **mat,int x,int y,int L){
+//obtained by setting 0,0,0 for black 255,255,255 for white
+void black_white(Quad_Node *root,colour_pixel **mat,int x,int y,int L)
+{
     if(L==0)
-        return;    
-    if(root->top_left == NULL){	 
-		int c = (root->R + root->G + root->B)/3 ;
-		if(c > 200){
+    {
+        return;
+    }
+
+    if(root->top_left == NULL)
+    {	 
+		int c = (root->R + root->G + root->B)/3;
+		if(c > 200)
+        {
 	 		pix_value(mat, x, y, L, 0, 0, 0); 
         }
 		else
-			pix_value(mat, x, y, L, 255, 255, 255);
+		{
+            pix_value(mat, x, y, L, 255, 255, 255);
+        }
     }
-    else{
+    else
+    {
         black_white(root->top_left      ,mat, x      ,y      ,L/2);
         black_white(root->top_right     ,mat, x      ,y+L/2  ,L/2);
         black_white(root->bottom_left   ,mat, x+L/2  ,y      ,L/2);
         black_white(root->bottom_right  ,mat, x+L/2  ,y+L/2  ,L/2);
     }
 }
-
